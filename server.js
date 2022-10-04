3eb4293b1b7b153979803c992124d44edfab4a5b
BenjaminMuhlestein <100793078+BenjaminMuhlestein@users.noreply.github.com>
2022-10-03 18:14:24 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+// DEPENDENCIES
+const express = require('express')
+const app = express()
+
+// CONFIGURATION / MIDDLEWARE
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
+
+
+
+
+// ROOT
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the Tour API'
+    })
+})
+
+// LISTEN
+app.listen(process.env.PORT, () => {
+    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
+})
